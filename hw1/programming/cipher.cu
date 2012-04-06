2c68f8521c2431fb07e1b0b0434c71026f63521d
D.B. Tsai <dbtsai@dbtsai.com>
2012-04-06 16:43:14 -0700

    add starter files for PA1

---
+++ hw1/programming/cipher.cu
@@ -0,0 +1,256 @@
+/* This is machine problem 1, part 1
+ * 
+ *         Shift Cipher
+ *
+ * The problem is to take in a string (a vector of characters) 
+ * and a shift amount, and add that number to each element of
+ * the string, effectively "shifting" each element in the 
+ * string.
+ * 
+ * We do this in four different ways:
+ * 1. With a standard cuda kernel loading chars and outputting chars for each thread
+ * 2. With a standard cuda kernel, casting the character pointer to an int so that
+ *    we load and store 4 bytes each time instead of 1 which gives us better coalescing
+ *    and uses the memory effectively to achieve higher bandwidth
+ * 3. Same spiel except with a uint2, so that we load 8 bytes each time
+ *
+ * Your job is to fill in all the places that are marked with TODO.
+ * If your code doesn't match the cpu, the check_results function
+ * will let you know.
+ * Do not change any function names, interfaces or existing variable names
+ *
+ * You should calculate the achieved memory bandwidth from the reported
+ * times.  Can you explain the results?  
+ * 
+ * Generate plots of (with char, uint, uint2 on the same plot):
+ * 1) Bandwidth vs. vector length
+ * 2) Bandwidth vs. block_size
+ *
+ * Explain the shape of each of these plots.
+ *
+ * What is the bandwidth the is achieved on the copies from
+ * the host to the device and back the other way?  How does
+ * this compare to the bandwidth achieved on the device?
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctime>
+#include <fstream>
+#include <vector>
+#include <thrust/device_vector.h>
+
+#include "mp1-util.h"
+
+
+// Repeating from the tutorial, just in case you haven't looked at it.
+
+// "kernels" or __global__ functions are the entry points to code that executes on the GPU
+// The keyword __global__ indicates to the compiler that this function is a GPU entry point.
+// __global__ functions must return void, and may only be called or "launched" from code that
+// executes on the CPU.
+
+void host_shift_cypher(std::vector<unsigned char> &input_array, std::vector<unsigned char> &output_array, unsigned char shift_amount)
+{
+  for(unsigned int i=0;i<input_array.size();i++)
+  {
+    unsigned char element = input_array[i];
+    output_array[i] = element + shift_amount;
+  }
+}
+
+// This kernel implements a per element shift
+// by naively loading one byte and shifting it
+__global__ void shift_cypher(unsigned char *input_array, unsigned char *output_array, unsigned char shift_amount,  unsigned int array_length)
+{
+    //TODO fill in
+}
+
+//Here we load 4 bytes at a time instead of just 1
+//to improve the bandwidth due to a better memory
+//access pattern
+__global__ void shift_cypher_int(unsigned int *input_array, unsigned int *output_array, unsigned int shift_amount, unsigned int array_length) 
+{
+    //TODO fill in
+}
+
+//Here we go even further and load 8 bytes
+//does it make a further improvement?
+__global__ void shift_cypher_int2(uint2 *input_array, uint2 *output_array, unsigned int shift_amount, unsigned int array_length) 
+{
+    //TODO fill in
+}
+
+bool checkResults(std::vector<unsigned char> &cipher_text_host, unsigned char *device_output_array,
+                  const char *type) {
+    //allocate space on host for gpu results
+    std::vector<unsigned char> cipher_text_from_gpu(cipher_text_host.size());
+
+    event_pair timer;
+    start_timer(&timer);
+    // download and inspect the result on the host:
+    cudaMemcpy(&cipher_text_from_gpu[0], device_output_array, cipher_text_host.size(), cudaMemcpyDeviceToHost);
+    check_launch("copy from gpu");
+    stop_timer(&timer,"copy from gpu");
+  
+    // check CUDA output versus reference output
+    int error = 0;
+    for(int i=0;i<cipher_text_host.size();i++)
+    {
+        if(cipher_text_host[i] != cipher_text_from_gpu[i]) 
+        { 
+            error = 1;
+            printf("Error at pos: %d\nexpected: %d got: %d\n", i, (int)cipher_text_host[i], (int)cipher_text_from_gpu[i]);
+            break;
+        }
+    }
+
+    if(error)
+    {
+        printf("Output of CUDA %s version and host version didn't match! \n", type);
+        return false;
+    }
+
+    return true;
+}
+
+int main(void)
+{
+
+  //First load the text 
+  std::ifstream ifs("mobydick.txt", std::ios::binary);
+  if (!ifs.good()) {
+      std::cerr << "Couldn't open book file!" << std::endl;
+      return 1;
+  }
+
+  std::vector<unsigned char> text;
+
+  ifs.seekg(0, std::ios::end); //seek to end of file
+  int length = ifs.tellg();    //get distance from beginning
+  ifs.seekg(0, std::ios::beg); //move back to beginning
+
+  text.resize(length);
+  ifs.read((char *)&text[0], length);
+
+  ifs.close();
+
+  //need to make a couple copies of the book, otherwise everything happens too quickly
+  //make 2^4 = 16 copies
+  for (int i = 0; i < 4; ++i) {
+      text.insert(text.end(), text.begin(), text.end());
+  }
+
+  // compute the size of the arrays in bytes
+  // with enough padding that a uint2 access won't be out of bounds
+  int num_bytes = (text.size() + 7) * sizeof(unsigned char);
+
+  //allocate host arrays
+  std::vector<unsigned char> cipher_text_gpu(text.size());
+  std::vector<unsigned char> cipher_text_host(text.size());
+
+  // pointers to device arrays
+  unsigned char *device_input_array  = 0;
+  unsigned char *device_output_array = 0;
+  
+  event_pair timer;
+  
+  // cudaMalloc device arrays
+  cudaMalloc((void**)&device_input_array,  num_bytes);
+  cudaMalloc((void**)&device_output_array, num_bytes);
+  
+  // if either memory allocation failed, report an error message
+  if(device_input_array == 0 || device_output_array == 0)
+  {
+    printf("couldn't allocate memory\n");
+    return 1;
+  }
+
+  // generate random input string
+  unsigned char shift_amount = (rand() % 25) + 1; //we don't want the shift to be 0!
+  
+  // do copies to and from gpu once to get rid of timing weirdness
+  // on first time accesses due to driver
+  // touch all memory
+  cudaMemcpy(device_input_array,  &text[0],            num_bytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(device_output_array, device_input_array,  num_bytes, cudaMemcpyDeviceToDevice);
+  cudaMemcpy(&text[0],            device_output_array, num_bytes, cudaMemcpyDeviceToHost);
+
+  start_timer(&timer);
+  // copy input to GPU
+  cudaMemcpy(device_input_array, &text[0], num_bytes, cudaMemcpyHostToDevice);
+  check_launch("copy to gpu");
+  stop_timer(&timer,"copy to gpu");
+  
+  // generate reference output
+  {
+      start_timer(&timer);
+      host_shift_cypher(text, cipher_text_host, shift_amount);
+      stop_timer(&timer,"host shift cypher");
+  }
+
+  // choose a number of threads per block
+  // we use 512 threads here
+  const int block_size = 512;
+
+  bool noErrors = true;
+  // generate GPU char output
+  {
+      int grid_size = -1; //TODO assign correct value
+      start_timer(&timer);
+      // launch kernel
+      //TODO call the kernel with the appropriate parameters here
+      //use the block_size defined above for the number of threads
+      check_launch("gpu shift cypher char");
+      stop_timer(&timer,"gpu shift cypher char");
+      if (!checkResults(cipher_text_host, device_output_array, "char")) {
+          noErrors = false;
+      }
+  }
+
+  // generate GPU uint output
+  {
+      int grid_size = -1; //TODO assign correct value
+      unsigned int iShift = -1; //TODO assign correct value; recommend using the left shift (<<) and bitwise OR (|) operators
+      start_timer(&timer);
+      // launch kernel
+      //TODO call the kernel with the appropriate parameters here
+      //use the block_size defined above for the number of threads
+      check_launch("gpu shift cypher uint");
+      stop_timer(&timer,"gpu shift cypher uint");
+      if (!checkResults(cipher_text_host, device_output_array, "uint")) {
+          noErrors = false;
+      }
+  }
+
+  //generate GPU uint2 output
+  {
+      int grid_size = -1; //TODO assign correct value
+      unsigned int iShift = -1; //TODO assign correct value; recommend using the left shift (<<) and bitwise OR (|) operators
+
+      start_timer(&timer);
+      // launch kernel
+      //TODO call the kernel with the appropriate parameters here
+      //use the block_size defined above for the number of threads
+      check_launch("gpu shift cypher uint2");
+      stop_timer(&timer,"gpu shift cypher uint2");
+      if (!checkResults(cipher_text_host, device_output_array, "uint2")) {
+          noErrors = false;
+      }
+  }
+
+  if (noErrors) {
+      printf("All CUDA Versions matched reference output.  Outputting ciphered text.\n");
+      std::ofstream ofs("mobydick_enciphered.txt");
+      //use the original length, before we made copies
+      for (int i = 0; i < length; ++i) {
+          ofs << cipher_text_host[i];
+      }
+      ofs.close();
+  }
+ 
+  // deallocate memory
+  cudaFree(device_input_array);
+  cudaFree(device_output_array);
+}
+
---
+++ hw1/programming/pagerank.cu
@@ -0,0 +1,224 @@
+/* This is machine problem 1, part 2
+ *
+ *            Page Ranking
+ *
+ * The problem is to compute the rank of a set of webpages
+ * given a link graph, aka a graph where each node is a webpage,
+ * and each edge is a link from one page to another.
+ * We're going to use the Pagerank algorithm (http://en.wikipedia.org/wiki/Pagerank),
+ * specifically the iterative algorithm for calculating the rank of a page
+ * We're going to run 20 iterations of the propage step.
+ * The CPU implementation is provided.  Write the CUDA version.
+ * Fill in all the places marked by TODO.
+ * Keep all function interfaces intact, don't change any existing
+ * variable names.
+ * 
+ * Your results are automatically checked against the CPU, make sure
+ * they match.
+ *
+ * From the time taken, calculate the achieved bandwidth (for the default
+ * of avg_edges=8).  How does this compare to the bandwidth from part 1?
+ * Explain any differences.
+ * Does changing the block_size make any difference? Why?
+ *
+ * Make a plot of bandwidth vs. avg_edges [2, 20], explain the shape
+ * of the curve.
+ *
+ * Does the size of the vector have a significant impact on the bandwidth?
+ * 
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <ctime>
+#include <limits>
+
+#include "mp1-util.h"
+
+event_pair timer;
+
+// amount of floating point numbers between answer and computed value 
+// for the answer to be taken correctly. 2's complement magick.
+const int maxUlps = 10;
+  
+void host_graph_propagate(unsigned int *graph_indices, unsigned int *graph_edges, float *graph_nodes_in, float *graph_nodes_out, float * inv_edges_per_node, int array_length)
+{
+  for(int i=0; i < array_length; i++)
+  {
+    float sum = 0.f; 
+    for(int j = graph_indices[i]; j < graph_indices[i+1]; j++)
+    {
+      sum += graph_nodes_in[graph_edges[j]]*inv_edges_per_node[graph_edges[j]];
+    }
+    graph_nodes_out[i] = 0.5f/(float)array_length + 0.5f*sum;
+  }
+}
+
+
+void host_graph_iterate(unsigned int *graph_indices, unsigned int *graph_edges, float *graph_nodes_A, float *graph_nodes_B, float * inv_edges_per_node, int nr_iterations, int array_length)
+{
+  assert((nr_iterations % 2) == 0);
+  for(int iter = 0; iter < nr_iterations; iter+=2)
+  {
+    host_graph_propagate(graph_indices, graph_edges, graph_nodes_A, graph_nodes_B, inv_edges_per_node, array_length);
+    host_graph_propagate(graph_indices, graph_edges, graph_nodes_B, graph_nodes_A, inv_edges_per_node, array_length);
+  }
+}
+
+// TODO your kernel code here
+__global__
+void device_graph_propagate(unsigned int *graph_indices, unsigned int *graph_edges, float *graph_nodes_in, float *graph_nodes_out, float *inv_edges_per_node, int array_length)
+{
+}
+
+//all of your gpu memory allocation and copying to gpu memory has to be in this function
+void device_graph_iterate(unsigned int *h_graph_indices,
+                          unsigned int *h_graph_edges,
+                          float *h_graph_nodes_input,
+                          float *h_graph_nodes_result,
+                          float *h_inv_edges_per_node,
+                          int nr_iterations,
+                          int num_elements,
+                          int avg_edges)
+{
+  unsigned int *d_graph_indices=0;
+  unsigned int *d_graph_edges=0;
+  float *d_graph_nodes_A=0;
+  float *d_graph_nodes_B=0;
+  float *d_inv_edges_per_node=0;
+
+  //TODO allocate memory
+
+  if (d_graph_indices == 0 || d_graph_edges == 0 || d_graph_nodes_A == 0||
+      d_graph_nodes_B == 0 || d_inv_edges_per_node == 0) {
+      printf("Couldn't allocate enough memory on the GPU!\n");
+      return;
+  }
+
+  //TODO copy memory to gpu
+
+  start_timer(&timer);
+
+  const int block_size = 128;
+  
+  // TODO your kernel calls
+  
+  check_launch("gpu graph propagate");
+  stop_timer(&timer,"gpu graph propagate");
+
+  // TODO your final result should end up in h_graph_nodes_result, which is a *host* pointer
+
+  cudaFree(d_graph_indices);
+  cudaFree(d_graph_edges);
+  cudaFree(d_graph_nodes_A);
+  cudaFree(d_graph_nodes_B);
+  cudaFree(d_inv_edges_per_node);
+}
+
+
+int main(void)
+{
+  // create arrays of 2M elements
+  int num_elements = 1 << 21;
+  int avg_edges = 8;
+  int iterations = 20;
+  
+  // pointers to host & device arrays
+  unsigned int *h_graph_indices = 0;
+  float *h_inv_edges_per_node = 0;
+  unsigned int *h_graph_edges = 0;
+  float *h_graph_nodes_input = 0;
+  float *h_graph_nodes_result = 0;
+  float *h_graph_nodes_checker_A = 0;
+  float *h_graph_nodes_checker_B = 0;
+  
+  // malloc host array
+  // index array has to be n+1 so that the last thread can 
+  // still look at its neighbor for a stopping point
+  h_graph_indices         = (unsigned int*)malloc((num_elements+1) * sizeof(unsigned int));
+  h_inv_edges_per_node    = (float*)       malloc((num_elements) * sizeof(float));
+  h_graph_edges           = (unsigned int*)malloc(num_elements * avg_edges * sizeof(unsigned int));
+  h_graph_nodes_input     = (float*)       malloc(num_elements * sizeof(float));
+  h_graph_nodes_result    = (float*)       malloc(num_elements * sizeof(float));
+  h_graph_nodes_checker_A = (float*)       malloc(num_elements * sizeof(float));
+  h_graph_nodes_checker_B = (float*)       malloc(num_elements * sizeof(float));
+  
+  // if any memory allocation failed, report an error message
+  if(h_graph_indices == 0 || h_graph_edges == 0 || h_graph_nodes_input == 0 || h_graph_nodes_result == 0 || 
+	 h_inv_edges_per_node == 0 || h_graph_nodes_checker_A == 0 || h_graph_nodes_checker_B == 0)
+  {
+    printf("couldn't allocate memory\n");
+    exit(1);
+  }
+
+  // generate random input
+  // initialize
+  srand(time(NULL));
+   
+  h_graph_indices[0] = 0;
+  for(int i=0;i< num_elements;i++)
+  {
+    int nr_edges = (i % (2*avg_edges-1) + 1);
+    h_inv_edges_per_node[i] = 1.f/(float)nr_edges;
+    h_graph_indices[i+1] = h_graph_indices[i] + nr_edges;
+    if(h_graph_indices[i+1] >= (num_elements * avg_edges))
+    {
+      printf("more edges than we have space for\n");
+      exit(1);
+    }
+    for(int j=h_graph_indices[i];j<h_graph_indices[i+1];j++)
+    {
+      h_graph_edges[j] = rand() % num_elements;
+    }
+    
+    h_graph_nodes_input[i] =  1.f/(float)num_elements;
+    h_graph_nodes_checker_A[i] =  h_graph_nodes_input[i];
+    h_graph_nodes_result[i] = std::numeric_limits<float>::infinity();
+  }
+  
+  //do page rank on the GPU
+  device_graph_iterate(h_graph_indices, h_graph_edges, h_graph_nodes_input, h_graph_nodes_result, h_inv_edges_per_node, iterations, num_elements, avg_edges);
+  
+  start_timer(&timer);
+  // generate reference output on CPU
+  host_graph_iterate(h_graph_indices, h_graph_edges, h_graph_nodes_checker_A, h_graph_nodes_checker_B, h_inv_edges_per_node, iterations, num_elements);
+  
+  check_launch("host graph propagate");
+  stop_timer(&timer,"host graph propagate");
+  
+  // check CUDA output versus reference output
+  int num_errors = 0;
+  for(int i=0;i<num_elements;i++)
+  {
+    float n = h_graph_nodes_result[i];
+    float c = h_graph_nodes_checker_A[i];
+    if(!AlmostEqual2sComplement(n,c,maxUlps)) 
+    {
+      num_errors++;
+      if (num_errors < 10)
+      {
+            printf("%d:%.16f::",i, n-c);
+      }
+    }
+  }
+  
+  if(num_errors > 0)
+  {
+    printf("Output of CUDA version and normal version didn't match! \n");
+  }
+  else
+  {
+    printf("Worked! CUDA and reference output match. \n");
+  }
+
+  // deallocate memory
+  free(h_graph_indices);
+  free(h_inv_edges_per_node);
+  free(h_graph_edges);
+  free(h_graph_nodes_input);
+  free(h_graph_nodes_result);
+  free(h_graph_nodes_checker_A);
+  free(h_graph_nodes_checker_B);
+}
+
